d284f2b7555de3e4598320db17fc6fdd16dbae23
Iwabuchi Ken <iwabuchi.k.2010@gmail.com>
2013-04-28 17:05:19 +0900

    B6
    v-3.0, 3.1
    ---
    1. 7-seg => On and off

---
+++ Sketches/D-6/v-3.0/C_1/C_1.ino
@@ -0,0 +1,28 @@
+/*
+  File: C_1
+  Behavior: LED turns on
+  Ports used:
+        13  => Output
+  Date: 2013/04/28 16:53:37
+
+*/
+
+const int LED = 13;    // Port 13 => Output (Connect the anode of the LED to this port)
+
+void setup()  
+{
+  
+    pinMode(LED, OUTPUT);
+    
+}
+
+void loop()
+{
+  
+    digitalWrite(LED, HIGH);
+    delay(500);
+
+    digitalWrite(LED, LOW);
+    delay(2000);
+  
+}
