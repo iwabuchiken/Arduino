89d5aa984cad9134f8bc347f04f7eaa19428123f
Iwabuchi Ken <iwabuchi.k.2010@gmail.com>
2013-05-28 12:50:21 +0900

    D-14
    v-3.0 e-2, e-3, e-3_t2
    ---
    1. Done

---
+++ Sketches/D-14/D_14_v_3_0_e_2_Cds_LED/D_14_v_3_0_e_2_Cds_LED.ino
@@ -0,0 +1,56 @@
+/*
+  File: D_14_v_2_0_e_2
+  Loc: Banzi.55
+  
+  Behavior: 
+  Ports used:
+        13  => Output
+  Date: 2013/05/28 11:36:26
+  
+  Memo: 
+
+*/
+/*****************************************
+*  Variables: Non-const
+******************************************/
+int val = 0;
+char dtostrfbuffer[15];
+
+/*****************************************
+*  Variables: Const
+******************************************/
+const int LED = 13;
+const int analog_read = 0;
+
+const float source_voltage = 4.96;
+const int max_range = 1023;
+
+/*****************************************
+*  Methods: Basics
+******************************************/
+void setup()                    // run once, when the sketch starts
+{
+    Serial.begin(9600);           // set up Serial library at 9600 bps
+    
+  //  Serial.println("Hello world!");  // prints hello with ending line break 
+    Serial.println("Started");  // prints hello with ending line break 
+    
+    pinMode(LED, OUTPUT);
+  
+}
+
+void loop()                       // run over and over again
+{
+    val = analogRead(analog_read);
+
+    Serial.print("val=");
+    Serial.println(String(val)); 
+//    Serial.println(dtostrf((val * source_voltage / max_range), 4, 2, dtostrfbuffer));
+    
+    digitalWrite(LED, HIGH);
+    delay(val);
+    
+    digitalWrite(LED, LOW);
+    delay(val);
+    
+}//void loop()
---
+++ Sketches/D-14/D_14_v_3_0_e_3_Cds_LED_brightness/D_14_v_3_0_e_3_Cds_LED_brightness.ino
@@ -0,0 +1,67 @@
+/*
+  File: D_14_v_3_0_e_3_Cds_LED_brightness
+  Loc: Banzi.55
+  
+  Behavior: 
+  Ports used:
+        13  => Output
+  Date: 2013/05/28 11:36:26
+  
+  Memo: 
+
+*/
+/*****************************************
+*  Variables: Non-const
+******************************************/
+int val = 0;
+int analog_output = 0;
+int divider = 4;
+
+char dtostrfbuffer[15];
+
+/*****************************************
+*  Variables: Const
+******************************************/
+const int LED = 13;
+const int analog_read = 0;
+
+const float source_voltage = 4.96;
+const int max_range = 1023;
+
+/*****************************************
+*  Methods: Basics
+******************************************/
+void setup()                    // run once, when the sketch starts
+{
+    Serial.begin(9600);           // set up Serial library at 9600 bps
+    
+  //  Serial.println("Hello world!");  // prints hello with ending line break 
+    Serial.println("Started");  // prints hello with ending line break 
+    
+    pinMode(LED, OUTPUT);
+  
+}
+
+void loop()                       // run over and over again
+{
+    val = analogRead(analog_read);
+    analog_output = val / divider;
+    
+    Serial.print("val=");
+    Serial.print(String(val) + "("); 
+//    Serial.print(dtostrf((source_voltage * (val / 4)/ max_range), 4, 2, dtostrfbuffer)); 
+    Serial.print(dtostrf((source_voltage * (analog_output)/ max_range), 4, 2, dtostrfbuffer)); 
+    Serial.print(")");
+
+    Serial.print("(analog_output="); 
+//    Serial.print(dtostrf((source_voltage * (val / 4)/ max_range), 4, 2, dtostrfbuffer)); 
+    Serial.print(String(analog_output)); 
+    Serial.print("=");
+    Serial.print(dtostrf((source_voltage * (analog_output)/ 255), 4, 2, dtostrfbuffer));
+    Serial.println(")");
+    
+//    analogWrite(LED, val / 4);
+    analogWrite(LED, analog_output);
+    delay(10);
+    
+}//void loop()
